7491dfec819b2b0e6ed58edd935b0785e2d80c97
吴家荣 <973430584@qq.com>
2021-08-30 15:25:17 +0800

    修复路径问题

--- atomcss-loader.js
+++ atomcss-loader.js
@@ -83,7 +83,7 @@ let oClassNameMap = {
 let oAtomConfig = {}
 
 try {
-  oAtomConfig = require(__dirname + '../../atomcss.config.js');
+  oAtomConfig = require(__dirname + '/../../atomcss.config.js');
 } catch (e) {
   oAtomConfig = require(__dirname + '/atomcss.config.js');
 }
